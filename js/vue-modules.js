ae8a0940c7e3c4036793846c42996fae95d26932
jenningshanna <jenningshanna@gmail.com>
2016-01-13 14:29:48 -0800

    working on forms

---
+++ js/vue-modules.js
@@ -0,0 +1,12 @@
+new Vue({
+	el: '#app',
+		data: {
+			'note': 'Your message',
+			'to': 'Dear . . .',
+			'addy1': 'Address 1',
+			'addy2': 'Address 2',
+			'city': 'City',
+			'state': 'State',
+			'zip': 'Zip'
+			}
+	})
