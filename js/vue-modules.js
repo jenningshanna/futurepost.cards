58c46090e3f4e898921aebeb11b22ca48f071dd5
jenningshanna <jenningshanna@gmail.com>
2018-04-01 21:58:00 -0700

    logic with vue

--- js/vue-modules.js
+++ js/vue-modules.js
@@ -11,3 +11,26 @@ new Vue({
 			'sign': ''
 			}
 	})
+	
+Vue.http.options.emulateJSON = true; // send as 
+
+new Vue({
+    el: '#postApp',
+    data: {
+        debug: true,
+        domain: '',
+        ajaxRequest: false,
+        postResults: []
+    },
+    methods: {
+      checkWebsite: function() {
+        this.ajaxRequest = true;
+        this.$http.post('https://wt-84851f78a8b77717e55137219dffeccd-0.run.webtask.io/stripe-payment', {
+              domain: this.domain
+            }, function (data, status, request) {
+                this.postResults = data;
+
+                this.ajaxRequest = false;
+            });
+      }}
+});
